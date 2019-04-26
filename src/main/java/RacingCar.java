1e775a0c307f3d4065ad439b56d5dbe6f0930e17
RudiaMoon <impactyoung@gmail.com>
2019-04-27 07:30:44 +0900

    Add Test 동적테스트 움직임 4이상 혹은 미만

---
+++ src/main/java/RacingCar.java
@@ -0,0 +1,11 @@
+public class RacingCar {
+
+    public static final int MOVE_MIN_COUNT = 4;
+    int moveCount;
+
+    public int move(int randomValue) {
+        if(randomValue >= MOVE_MIN_COUNT )
+           return moveCount+=1;
+        return moveCount;
+    }
+}
---
+++ src/test/java/RacingCarTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class RacingCarTest {
+    @Test
+    public void move_동작_테스트_움직임_조건_4미만인_경우_정지() {
+        RacingCar car = new RacingCar();
+
+        int result = car.move(3);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void move_동작_테스트_움직임_조건_4이상인_경우_이동() {
+        RacingCar car = new RacingCar();
+
+        int result = car.move(4);
+        assertThat(result).isEqualTo(1);
+    }
+}
