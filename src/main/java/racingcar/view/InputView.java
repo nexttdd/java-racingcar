01b3885e4bcca021bf55a99c1359feb870768ac0
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-04-27 11:38:04 +0900

    #피드백반영 - 불필요한 공백제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,7 +3,6 @@ package racingcar.view;
 import java.util.Scanner;
 
 public class InputView {
-
     public static Integer inputNumberOfCars() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("자동차 대수는 몇 대 인가요?");
