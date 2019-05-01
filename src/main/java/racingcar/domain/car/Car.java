63d1e30ce6e1a598ed53ba87c9aede815212c97b
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-05-01 18:14:16 +0900

    #임시커밋

--- src/main/java/racingcar/domain/car/Car.java
+++ src/main/java/racingcar/domain/car/Car.java
@@ -3,9 +3,15 @@ package racingcar.domain.car;
 public class Car {
     private static final int DEFAULT_POSITION = 0;
 
+    private CarName carName;
     private int position;
 
     public Car(int position) {
+        this.position = position;
+    }
+
+    public Car(String name, int position) {
+        this.carName = new CarName(name);
         this.position = position;
     }
 
@@ -17,8 +23,20 @@ public class Car {
         return new Car(position);
     }
 
+    public static Car generate(String name) {
+        return new Car(name, DEFAULT_POSITION);
+    }
+
+    public static Car generateWithPosition(String name, int position) {
+        return new Car(name, position);
+    }
+
     public void move(int distance) {
         this.position += distance;
+    }
+
+    public String getCarName() {
+        return this.carName.toString();
     }
 
     public int getPosition() {
