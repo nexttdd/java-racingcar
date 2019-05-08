2c3b153496d4d2ec85f2232f3a53b346320f6fdc
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-05-09 08:49:17 +0900

    #step2 피드백반영 - Scanner 중복사용 제거, 미사용코드 제거

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,20 +3,15 @@ package racingcar.view;
 import java.util.Scanner;
 
 public class InputView {
-    public static Integer inputNumberOfCars() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("자동차 대수는 몇 대 인가요?");
-        return Integer.parseInt(scanner.nextLine());
-    }
+
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static String inputRacingCars() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
         return scanner.nextLine();
     }
 
     public static Integer inputTimes() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("시도할 회수는 몇 회 인가요?");
         return Integer.parseInt(scanner.nextLine());
     }
